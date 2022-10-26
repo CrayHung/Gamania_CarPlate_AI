1d88050e40f187081644be35a5151a9db00e78a0
owlsinya <owlsinya@gmail.com>
2022-10-26 15:20:52 +0800

    車辨頁面版型更新-2

--- Gamania_CarPlate_AI-frontend/src/live/LiveVideo.js
+++ Gamania_CarPlate_AI-frontend/src/live/LiveVideo.js
@@ -10,44 +10,62 @@ export default function LiveVideo() {
       </div>
 
       <div className="grid">
-        <div className="grid-item item1 ">
-          <div className="col">機車</div>
-        </div>
-        <div className="grid-item item2">
-          <div className="col">汽車</div>
-        </div>
         <div className="grid-item item3">
-          <div style={{ width: 250, height: 250, textAlign: "end" }}>
+          <div style={{ width: "100%", height: 250, textAlign: "center" }}>
             <h2>機車即時畫面</h2>
           </div>
         </div>
         <div className="grid-item item4">
-          <div className="col">汽車即時畫面</div>
+          <div className="col">
+            <h2>汽車即時畫面</h2>
+          </div>
         </div>
         <div className="grid-item item4">
-          <div className="col">汽車即時畫面</div>
+          <div className="col">
+            <h2>汽車即時畫面</h2>
+          </div>
         </div>
         <div className="grid-item item4">
-          <div className="col">汽車即時畫面</div>
+          <div className="col">
+            <h2>汽車即時畫面</h2>
+          </div>
         </div>
       </div>
-      <div className="grid grid-item grid-title">
+      <div className="grid-item grid-title">
         <div className="col">車辨畫面</div>
       </div>
       <div className="grid">
         <div className="grid-item item3">
-          <div style={{ width: 250, height: 250, textAlign: "end" }}>
+          <div style={{ width: "100%", height: 250, textAlign: "center" }}>
             <h2>機車車辨畫面</h2>
           </div>
         </div>
         <div className="grid-item item4">
-          <div className="col">汽車車辨畫面</div>
+          <div className="col">
+            <h2>汽車車辨畫面</h2>
+          </div>
         </div>
         <div className="grid-item item4">
-          <div className="col">汽車車辨畫面</div>
+          <div className="col">
+            <h2>汽車車辨畫面</h2>
+          </div>
         </div>
         <div className="grid-item item4">
-          <div className="col">汽車車辨畫面</div>
+          <div className="col">
+            <h2>汽車車辨畫面</h2>
+          </div>
+        </div>
+        <div className="grid-item item1">
+          <div className="col">車牌號碼</div>
+        </div>
+        <div className="grid-item item1">
+          <div className="col">車牌號碼</div>
+        </div>
+        <div className="grid-item item1">
+          <div className="col">車牌號碼</div>
+        </div>
+        <div className="grid-item item1">
+          <div className="col">車牌號碼</div>
         </div>
       </div>
     </div>
