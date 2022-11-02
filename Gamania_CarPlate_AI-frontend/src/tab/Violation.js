3947fffda67481b79258222abc62162c68ad3c9d
owlsinya <owlsinya@gmail.com>
2022-11-02 16:44:03 +0800

    SearchTable 功能新增

--- Gamania_CarPlate_AI-frontend/src/tab/Violation.js
+++ Gamania_CarPlate_AI-frontend/src/tab/Violation.js
@@ -1,20 +1,32 @@
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import ViolationDemo from "../violation/ViolationDemo";
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from "react-intl";
+import React, { createContext, useState, useContext } from "react";
+
+export const TableContext = createContext();
+
+function TableProvider({ children }) {
+  const [tableData, setTableData] = useState([]);
+
+  return (
+    <TableContext.Provider value={{ tableData, setTableData }}>
+      {children}
+    </TableContext.Provider>
+  );
+}
 
 export default function Violation() {
   return (
     <HelmetProvider>
       <Helmet>
-        <title>
-          {useIntl().formatMessage({id: "violation-title"})}
-        </title>
+        <title>{useIntl().formatMessage({ id: "violation-title" })}</title>
       </Helmet>
       <div className="main-title">
-       <FormattedMessage id="record-table-title"></FormattedMessage>
+        <FormattedMessage id="record-table-title"></FormattedMessage>
       </div>
-      <ViolationDemo/>
-
+      <TableProvider>
+        <ViolationDemo />
+      </TableProvider>
     </HelmetProvider>
-  )
+  );
 }
--- Gamania_CarPlate_AI-frontend/src/violation/ViolationDemo.js
+++ Gamania_CarPlate_AI-frontend/src/violation/ViolationDemo.js
@@ -1,76 +1,61 @@
 /***
- * 
- * 
+ *
+ *
  * fetch    http://192.168.195.213:8080/lpr/all
- * 
+ *
  * 圖片     http://192.168.195.213:8080/jpg/20221027_154658894_RCV-5981.jpg      (將./jpg/20221027_154658894_RCV-5981.jpg前面的.去掉)
  *          上傳之前要把 192.168.195.213 > 192.168.195.213
  */
 import ReactTable from "./table/ReactTable";
-import { useState, useEffect,useContext} from "react";
+import { useState, useEffect, useContext } from "react";
 import { FormattedMessage } from "react-intl";
 import ImgButton from "./table/ImgButton";
-import {urlContext} from '../web/Root'
+import { urlContext } from "../web/Root";
 import { CurrentTimeDisplay } from "video-react";
 import TimelineItem from "antd/lib/timeline/TimelineItem";
-
-
+import SearchBar from "./search/SearchBar";
+import { TableContext } from "../tab/Violation";
 
 export default function ViolationDemo() {
-  const [tableData, setTableData] = useState("");
+  // const [tableData, setTableData] = useState("");
+  const { tableData, setTableData } = useContext(TableContext);
   const [tmpData, setTmpData] = useState("");
 
   const serverUrl = useContext(urlContext);
   // console.log('serverUrl in violationDemo')
   // console.log(serverUrl)
 
-  const fetchurl = serverUrl+"lpr/all";
+  const fetchurl = serverUrl + "lpr/all";
+  // const fetchurl = "http://192.168.195.213:8080/lpr/all";
 
   useEffect(() => {
-
     const fetchData = async () => {
+      try {
+        const response = await fetch(fetchurl);
+        const data = await response.json();
 
-
-        try {
-            const response = await fetch(fetchurl);
-            const data = await response.json();
-
-
-
-              for (let i = 0; i < data.length; i++) {
-                const eTime0 = data[i]["imagePath"].replace("./", "");
-                data[i]["imagePath"] = eTime0;
-              }
-              setTableData(data);
-              //setTableData([...tableData,data]);
-
-
-            
-        } catch (error) {
-            console.log("error", error);
+        for (let i = 0; i < data.length; i++) {
+          const eTime0 = data[i]["imagePath"].replace("./", "");
+          data[i]["imagePath"] = eTime0;
         }
+        setTableData(data);
+        //setTableData([...tableData,data]);
+      } catch (error) {
+        console.log("error", error);
+      }
 
-        //每10秒重新抓一次資料
-        // setInterval(() => {
-        //   fetchData();
-        //  }, 10000);
-
+      //每10秒重新抓一次資料
+      // setInterval(() => {
+      //   fetchData();
+      //  }, 10000);
     };
 
     fetchData();
-
-}, []);
-
-
-
-
-
+  }, []);
 
   return (
     <div className="App">
-      <h1>
-        <FormattedMessage id="record-table-title" />
-      </h1>
+      <SearchBar />
       <ReactTable
         tableData={tableData}
         sizePerPage={sizePerPage}
@@ -81,7 +66,6 @@ export default function ViolationDemo() {
   );
 }
 
-
 const sizePerPage = 10;
 
 const TableHeader = () => {
@@ -90,39 +74,37 @@ const TableHeader = () => {
       <th>
         <FormattedMessage id="visitor-plateNumber" />
       </th>
+      <th>姓名</th>
+      <th>車種</th>
       <th>
         <FormattedMessage id="recognitionTimeStr" />
       </th>
       <th>
-        <FormattedMessage id="passStatus"  />
+        <FormattedMessage id="passStatus" />
       </th>
       <th>
-        <FormattedMessage id="cameraId"  />
+        <FormattedMessage id="cameraId" />
       </th>
 
       <th>
         <FormattedMessage id="table-th7" />
       </th>
-
-
-
     </tr>
   );
 };
 
 const tableBody = (value, index) => {
-
   return (
     <tr key={index}>
       <td>{value.plateNumber}</td>
+      <td>{value.name}</td>
+      <td>{value.vehicleType}</td>
       <td>{value.recognitionTimeStr}</td>
       <td>{value.passStatus}</td>
       <td>{value.cameraId}</td>
       <td>
         <ImgButton imagePath={value.imagePath} />
       </td>
-
     </tr>
   );
 };
-
